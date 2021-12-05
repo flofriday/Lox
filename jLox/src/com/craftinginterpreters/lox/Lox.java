dc8c00d100d724a2a3482a15b566f50d723188fe
flofriday <flohacksfriday@gmail.com>
2021-12-05 13:26:56 +0100

    Implement the jLox interpreter till chapter 5.0 The scanner is done and it is very exciting to watch it chomp through your code. I know its just tokens but still, there is something magical about it.

--- jLox/src/com/craftinginterpreters/lox/Lox.java
+++ jLox/src/com/craftinginterpreters/lox/Lox.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
