ec747d7cde8d1330a9d41533f789ce2b0f70edc8
flofriday <flohacksfriday@gmail.com>
2022-01-09 20:03:18 +0100

    Implement the cLox interpreter till chapter 23.4

--- cLox/vm.c
+++ cLox/vm.c
@@ -86,6 +86,7 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_SHORT() (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op)                        \
     do                                                  \
@@ -185,10 +186,10 @@ static InterpretResult run()
             break;
         }
         case OP_GREATER:
-            BINARY_OP(BOOL_VAL, <);
+            BINARY_OP(BOOL_VAL, >);
             break;
         case OP_LESS:
-            BINARY_OP(BOOL_VAL, >);
+            BINARY_OP(BOOL_VAL, <);
             break;
         case OP_ADD:
         {
@@ -233,6 +234,25 @@ static InterpretResult run()
             printValue(pop());
             printf("\n");
             break;
+        case OP_JUMP:
+        {
+            uint16_t offset = READ_SHORT();
+            vm.ip += offset;
+            break;
+        }
+        case OP_JUMP_IF_FALSE:
+        {
+            uint16_t offset = READ_SHORT();
+            if (isFalsey(peek(0)))
+                vm.ip += offset;
+            break;
+        }
+        case OP_LOOP:
+        {
+            uint16_t offset = READ_SHORT();
+            vm.ip -= offset;
+            break;
+        }
         case OP_RETURN:
             // Exit interpreter
             return INTERPRET_OK;
@@ -241,6 +261,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_SHORT
 #undef READ_STRING
 #undef BINARY_OP
 }
